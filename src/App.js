c83dba7660abdcf96fb084e0355bb4821105f4b7
Gideon-Cyber <gideonjohnson70@gmail.com>
2023-03-20 02:05:40 +0100

    added schedular component from a package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import Calendar from "./components/Calendar";
+import {
+  Inject,
+  ScheduleComponent,
+  Day,
+  Week,
+} from "@syncfusion/ej2-react-schedule";
 
 function App() {
   return (
     <div className="App text-white overflow-hidden">
-      <Calendar />
+      {/* <Calendar /> */}
+      <ScheduleComponent
+        currentView="Week"
+        selectedDate={new Date()}
+        eventSettings={{ dataSource: [] }}
+      >
+        <Inject services={[Day, Week]} />
+      </ScheduleComponent>
     </div>
   );
 }
